9e86c027bbc4aaa4699faa399704042b9c617d34
oyo <oyo@oyos-MacBook-Pro.local>
2024-04-22 19:40:01 +0530

    l

--- components/Task.js
+++ components/Task.js
@@ -3,10 +3,11 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Task = (props) => {
 
-  return (
+  return ( 
     <View style={styles.item}>
       <View style={styles.itemLeft}>
         <View style={styles.square}></View>
+      
         <Text style={styles.itemText}>{props.text}</Text>
       </View>
       <View style={styles.circular}></View>
